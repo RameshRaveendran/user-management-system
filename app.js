3e3c77893a25b9d07a2fab4b599ccfd2746737b1
Ramesh Raveendran <rameshraveendranm@gmail.com>
2025-11-23 15:04:36 +0530

    session and cookie added

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 
 // core & third-party imports
 const express = require('express');
+// session configuration
+const session = require('express-session');
+const  MongoStore = require('connect-mongo');
+
 
 // local imports (DB)
 const connectDB = require('./db/connect');
+const cookieParser = require('cookie-parser');
 
 
 // app initialization
@@ -16,7 +21,31 @@ const PORT = process.env.PORT || 3000;
 // implement db
 connectDB();
 
+// global middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(require('nocache')());
 
+// session handling
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,              // session secret key
+    resave: false,                                   // do not save if nothing changed
+    saveUninitialized: false,                        // do not create empty sessions
+
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,               // store sessions in MongoDB
+      collectionName: 'sessions',
+    }),
+
+    cookie: {
+      httpOnly: true,                                 // JS cannot access cookie
+      maxAge: 1000 * 60 * 60 * 24,                    // 1 day
+      secure: process.env.NODE_ENV === "production",  // only https in prod
+    }
+  })
+);
 
 
 
